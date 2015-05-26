13494bcb6d38f8c6b3f6902932f8693de81e4fae
rjf <runjf@qq.com>
2015-05-26 18:46:32 +0800

    修改测试代码

--- src/test/java/org/rongji/dfish/misc/pinyin/PinyinHelperTest.java
+++ src/test/java/org/rongji/dfish/misc/pinyin/PinyinHelperTest.java
@@ -5,12 +5,25 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PinyinHelperTest {
-
+	
+	private String str = "厦门市少年有不少参加了马拉松比赛";
+	
 	@Test
-	public void test() {
-//		fail("Not yet implemented");
-		String s=PinyinHelper.convertToPinyinString("厦门市少年有不少参加了马拉松比赛","",PinyinHelper.WITH_TONE_MARK);
+	public void testWithToneMark() {
+		String s=PinyinHelper.convertToPinyinString(str, "", PinyinHelper.WITH_TONE_MARK);
 		assertEquals("xiàménshìshàoniányoǔbùshǎocānjiālemǎlāsōngbǐsài", s);
+	}
+	
+	@Test
+	public void testWithToneNumber() {
+		String s=PinyinHelper.convertToPinyinString(str, "", PinyinHelper.WITH_TONE_NUMBER);
+		assertEquals("xia4men2shi4shao4nian2you3bu4shao3can1jia1le5ma3la1song1bi3sai4", s);
+	}
+	
+	@Test
+	public void testWithoutTone() {
+		String s=PinyinHelper.convertToPinyinString(str, "", PinyinHelper.WITHOUT_TONE);
+		assertEquals("xiamenshishaonianyoubushaocanjialemalasongbisai", s);
 	}
 
 }
